43713a478729062902834746f8433869b837c2b8
Ronicle <83679686+Ronicle@users.noreply.github.com>
2021-06-02 19:53:00 -0300

    Primeiro upload

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('#slider').bxSlider({
+        auto: true,
+        controls: false,
+    });
+
+    $("#but-menu"). any (function(){
+        $("#menu").slideToggle()
+    })
+});
